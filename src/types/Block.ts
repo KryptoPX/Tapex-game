ba0d1b111ca09c0b4c737e60e511791b42dc4983
KurtPX <pixelbit2000@gmail.com>
2021-07-20 21:39:58 +0200

    Define Block

--- src/types/Block.ts
+++ src/types/Block.ts
@@ -1,6 +1,7 @@
 class Block {
-    Type: BlockType;    // Tipo de bloque
-    value: number;      // Valor del bloque
+    Type: BlockType;                // Tipo de bloque
+    value: number;                  // Valor del bloque
+    iluminate: boolean = false;     // Define si el bloque se ilumina
     constructor(type: BlockType, num: number) {
         this.Type = type;
         this.value = num;
